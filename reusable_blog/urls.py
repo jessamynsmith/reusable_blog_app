d99a200ad024fa3b23ab435c51b5ba5be0b32766
Mike Hibbert <hibbert.michael@gmail.com>
2015-10-12 14:57:17 +0100

    Update urls.py

--- reusable_blog/urls.py
+++ reusable_blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^posts/$', views.blog_posts, name='blog_posts'),
-    url(r'^(?P<id>\d+)/$', views.post_detail, name='blog_post'),
+    url(r'^posts/(?P<id>\d+)/$', views.post_detail, name='blog_post'),
     url(r'^post/new/$', views.new_post, name='new_post'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.edit_post, name='edit_post'),
 
